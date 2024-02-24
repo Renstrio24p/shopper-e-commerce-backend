e4e0f9d919e0dfb87b6e0daf677ceecf1d749b20
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2024-02-25 06:51:22 +0800

    add deploy

---
+++ esbuild.config.js
@@ -0,0 +1,28 @@
+
+const esbuild = require('esbuild')
+const copy = require('esbuild-copy-static-files')
+
+esbuild.build({
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    minify: true,
+    outdir: 'dist',
+    lineLimit: 50,
+    splitting: true,
+    ignoreAnnotations: true,
+    format: 'esm',
+    platform: 'node',
+    loader: {
+        '.png':'copy',
+        '.ts':'ts'
+    },
+    plugins: [
+        copy({
+            src: './upload/images',
+            dest: './dist/upload/images',
+            recursive: true,
+            minify: true,
+            target: 'es5'
+        })
+    ]
+})
